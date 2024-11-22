e3f3d6444d8c3cfd836f179fe8cfee9e05c77eb4
yousef <yosifsamir178@gmail.com>
2024-11-23 01:10:48 +0200

    initial commit

--- app/src/main/java/com/example/myapplication/FileFragment.kt
+++ app/src/main/java/com/example/myapplication/FileFragment.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication
 
 import android.content.Context.MODE_PRIVATE
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.databinding.FragmentFileBinding
 import com.example.myapplication.databinding.FragmentFirstBinding
+import java.io.File
+import java.io.FileOutputStream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,6 +60,43 @@ class FileFragment : Fragment() {
             val text = readFromFile()
             binding.messageTxt.text = text
         }
+
+        binding.saveTextToExternalStorageBtn.setOnClickListener {
+            val text = binding.messageInputEdtTxt.text
+            text?.let {
+                writeToExternalStorage(text.toString())
+            }
+        }
+
+    }
+
+    private fun writeToExternalStorage(text: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            Log.d("dddd" , "Environment.isExternalStorageLegacy() =>" + Environment.isExternalStorageLegacy().toString())
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Log.d("dddd" , "Environment.isExternalStorageManager() =>" + Environment.isExternalStorageManager().toString())
+        }
+        Log.d("dddd" , "Environment.isExternalStorageRemovable() =>" + Environment.isExternalStorageRemovable())
+
+        val dataDirectory = Environment.getDataDirectory()
+        Log.d("dddd", "dataDirectory: $dataDirectory")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val storageDirectory = Environment.getStorageDirectory()
+            Log.d("dddd", "storageDirectory: $storageDirectory")
+        }
+        val externalStorageState = Environment.getExternalStorageState()
+        Log.d("dddd", "externalStorageState: $externalStorageState")
+
+
+//
+//        val externalCacheDir = requireContext().externalCacheDir
+//        Log.d("FileFragment", "externalCacheDir: $externalCacheDir")
+//        val file = File(externalCacheDir, "TTTT.txt")
+////        file.writeText(text)
+//        FileOutputStream(file, true).use {
+//            it.write(text.toByteArray())
+//        }
 
     }
 
